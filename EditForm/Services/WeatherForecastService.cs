bd521fefccd12277e8ad3a6a9f972d4d91b5ef06
Fadi Abji <fadi@wexer.se>
2024-08-05 13:26:35 +0200

    Add project files.

---
+++ EditForm/Services/WeatherForecastService.cs
@@ -0,0 +1,27 @@
+﻿using EditForm.Data;
+
+namespace EditForm.Services
+{
+    public class WeatherForecastService
+    {
+        public WeatherForecastService()
+        {
+                
+        }
+
+        public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+
+            // Simulate asynchronous loading to demonstrate a loading indicator
+            await Task.Delay(500);
+
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
